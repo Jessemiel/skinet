67488fcb10e5ec7c0828b8539617005729d2f03b
Jessemiel Adraneda <clericalsoft-dev@frontiertowersphilippines.com>
2025-09-04 11:27:56 +0800

    Implements product repository pattern
    Introduces a repository pattern for managing product data, improving separation of concerns and testability.
    
    This change refactors the ProductsController to use an IProductRepository interface for data access, allowing for easier swapping of data sources and mocking in unit tests. It includes methods for retrieving, adding, updating, and deleting products, as well as fetching brands and types. Adds seed data to initialize the database.

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Core.Interface;
 
 var builder = WebApplication.CreateBuilder(args);
 //SERVICES
@@ -7,12 +8,29 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(opt =>
-{ 
+{
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 //MIDDLEWARES
 app.MapControllers();
+
+try
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    throw;
+}
 app.Run();
